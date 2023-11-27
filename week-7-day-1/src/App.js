0543b0323ab68ea9afdbf19f6d1c4268d434abdc
FarhathH <farhathh0@gmail.com>
2023-11-27 13:38:31 +0000

    adding props to change the background colour

--- week-7-day-1/src/App.js
+++ week-7-day-1/src/App.js
@@ -1,20 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import ColourButton from './ColourButton.jsx';
-import {useState} from 'react';
+import ColourButton from './ColourButton.jsx'; //exported the button file
+import {useState} from 'react'; //to allow us to use useState function
 
 function App() {
 
+  //list and background for colours
   const[colourList, SetColourList]=useState(["black", "hotpink", "blue", "orange", "limegreen", "red"]);
+  const[backGround, setBackGround]=useState('#282c34');
+
+
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style = {{backgroundColor:backGround}}> {/*background colour is set to */}
 
         {colourList.map((col)=>{
           return(
             <div>
-              <ColourButton colour = {col}/>
+              <ColourButton colour = {col} newColorBackground = {setBackGround}/> {/*changes background when button is clicked*/}
             </div>
           );
         })}
--- week-7-day-1/src/ColourButton.jsx
+++ week-7-day-1/src/ColourButton.jsx
@@ -4,18 +4,13 @@ import './ColourButton.css'//my stylesheet
 function ColourButton(props){ //function for the button
 
     
-    const[col, setCol] = useState(props.colour);
-
-    function buttonClick(){
-        return(
-            <header style = {{backgroundColor:col}}></header>
-        );
-    };
-
+    const[col, setCol] = useState(props.colour); //useState for the colour button
+   
+   {/*this is executed on the page*/}
     return(
-        
+        //onclick function using the props object to change the colour of the background
         <div> 
-            <button onClick={buttonClick} className = "btn" style = {{backgroundColor:col}}>
+            <button className = "btn" style = {{backgroundColor:col}} onClick={()=>{props.newColorBackground(col)}}>
                 {col}
             </button>    
         </div>
