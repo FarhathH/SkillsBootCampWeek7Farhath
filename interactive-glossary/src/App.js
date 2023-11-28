8e226c01564381ad48a6570115cf51630829490e
FarhathH <farhathh0@gmail.com>
2023-11-28 11:10:34 +0000

    adding in images

--- interactive-glossary/src/App.js
+++ interactive-glossary/src/App.js
@@ -4,34 +4,45 @@ import './glossary.css';
 import {useState} from 'react';
 import GlossaryButton from './glossary-button';
 
+//imported my images from my file to be put in an array.
+import sequence from './images/sequencing.jpg';
+import selection from './images/selection.jpg';
+import iteration from './images/iteration.jpg';
+import string from './images/string.jpg';
+
 function App() {
   
   //array for iterating the button names and info
   const[buttonNames, setButtonNames] = useState(["Sequencing", "Selection", "Iteration", "String"]);
   //array for objects to display on the left side of web page
+  //have also inserted
   const[concepts, setConcepts] = useState(
     [
       {
         title:"Sequencing",
-        description: "the coding that determines the logical flow of the program."
+        description: "the coding that determines the logical flow of the program.",
+        picture:sequence
       },
       {
         title:"Selection",
-        description:"The second control structure. Selectively executing a block of code depending on whether the condition is true or not."
+        description:"The second control structure. Selectively executing a block of code depending on whether the condition is true or not.",
+        picture:selection
       },
       {
         title: "Iteration",
-        description:"The third control structure. Continuously executing a block of code if the condition is true."
+        description:"The third control structure. Continuously executing a block of code if the condition is true.",
+        picture:iteration
       },
       {
         title:"String",
-        description:"The data type that stores mutiple characters."
+        description:"The data type that stores mutiple characters.",
+        picture:string
       }
     ]
   );
   
   //stores the title and description objects
-  const[selectedItem, setSelectedItem] = useState({title:"", description:""})
+  const[selectedItem, setSelectedItem] = useState({title:"", description:"", picture:""})
 
   function OnButtonClick(index){
     setSelectedItem(concepts[index])
@@ -44,14 +55,19 @@ function App() {
           <div className = "info">
             <div className = "info-container">
               <div className = "paragraph">
-                <h2>{selectedItem.title}</h2>
-                <p>{selectedItem.description}</p>
+                <h2 className = "title">{selectedItem.title}</h2>
+                <p className = "describe">{selectedItem.description}</p>
               </div>
-              <img className = "pic"></img>
+              <div className = "pic">
+                <img width = "90%" height = "90%" src = {selectedItem.picture}></img> {/**/}
+              </div>
             </div>
           </div>
+          <div className = "line">
+              <div className = "barrier"></div> {/*creates a barrier separating the info and button menu*/}
+            </div>
           <div className = "btn-menu">
-            {concepts.map((btn, index)=>{ //iterating buttons with parameters
+            {concepts.map((btn, index)=>{ //iterating buttons and info with parameters
               return(
                 <>
                   <GlossaryButton name = {btn.title} action = {OnButtonClick} index = {index}/> {/*a click calls the index of the title and description*/}
